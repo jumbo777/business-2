54e5e68684af051736736f1ed9670d0956cfa41f
jumbo777 <jmwaniki93@gmail.com>
2020-02-12 22:29:04 +0300

    Email text

--- js/custom.js
+++ js/custom.js
@@ -18,7 +18,7 @@ $(document).ready(function()
         var buttonText = $('button.contact#main-button').text();
         if (buttonText === 'back')
         {
-            buttonText = 'click to get in touch';
+            buttonText = 'Send a Quick Email';
             $('button.contact#main-button').text(buttonText);
         }
         else
